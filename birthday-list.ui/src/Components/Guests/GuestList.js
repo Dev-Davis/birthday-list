154790d1b0f0ce112228f28890e2cd5510a1376b
Sean Davis <sean.dev.davis@gmail.com>
2020-03-02 20:02:34 -0600

    WIP

--- birthday-list.ui/src/Components/Guests/GuestList.js
+++ birthday-list.ui/src/Components/Guests/GuestList.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import './GuestList.css'
 
 class GuestList extends React.Component {
   render() {
     const { guest } = this.props;
     return (
-      <div className="the-list col-lg-6 offset-3">
+      <div className="container">
         <ul className="col-12">
-        <h6 className="invite">Guest #{guest.id}</h6>
-        <p>Name: {guest.firstName} {guest.lastName}</p>
-        <p>Address: {guest.address}</p>
-        <p>Relationship: {guest.relationship}</p>
-        <p>City: {guest.city}</p>
-        <p>State: {guest.state}</p>
+          <h6 className="invite">Guest #{guest.id}</h6>
+          <li>Name: {guest.firstName} {guest.lastName}</li>
+          <li>Address: {guest.address}</li>
+          <li>Relationship: {guest.relationship}</li>
+          <li>City: {guest.city}</li>
+          <li>State: {guest.state}</li>
         </ul>
       </div>
     )
